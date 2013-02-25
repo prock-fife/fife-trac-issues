2d32ff85ba8d39b05b1d852f38df71d5f1bbcbfe
Wayne Prasek <wprasek@gmail.com>
2013-02-25 13:42:21 -0600

    Dumping comments now works.

--- convert.py
+++ convert.py
@@ -25,8 +25,10 @@
 import collections
 import csv
 import datetime
+from DateTime import DateTime
 import json
 import urllib
+import pickle 
 
 from data import DEFAULT_USER, USERNAMES, MILESTONES, STATES, LABELS
 
@@ -34,49 +36,19 @@ from data import DEFAULT_USER, USERNAMES, MILESTONES, STATES, LABELS
 # Configuration. Who doesn't like configuration.
 #######################################################################
 # Your repository name (the subdirectory we will write data to).
-REPO = 'unknown-horizons/'
+REPO = 'fife/'
 # Locations of particular files. It is recommended to keep issues
 # and comments  in the same directory, which usually is issues/.
 ISSUES_PATH = REPO + 'issues/%s.json'
 COMMENTS_PATH = REPO + 'issues/%s.comments.json'
 MILESTONES_PATH = REPO + 'milestones/%s.json'
-# Path to csv-exported comments. We used the following sql (trac 0.12):
-# SELECT `ticket`, `time`, `author`, `newvalue` FROM `ticket_change`
-# WHERE `field`='comment' AND `newvalue` ORDER BY `time` ASC
-CSVFILE = './comments.csv'
-# This depends on your database csv export routine.
-CSVDELIM = ','
-CSVESCAPE ='\''
-# Mainly important to get right is this ID. Make sure it has all data
-# you need. Trac usually calls the report something along the lines of
-# "all tickets by milestone (Including closed)".
-REPORT_NO = 6
+# Path to the ticket pickle file
+TICKET_FILE = REPO + 'tickets.p'
 #
 # Magic report id we used to attach special labels to certain tickets
 # As all EASY_ stuff, commented out but we used it just like that.
 #EASY_NO = 14
 #
-# A valid url that opens the trac main page.
-TRAC_URL = 'http://trac.unknown-horizons.org/t/'
-#
-# If you use a custom server configuration or an older or more recent
-# trac version, you may or may not have to adapt the following lines.
-TRAC_TICKET_URL = TRAC_URL + 'ticket/%s'
-TRAC_REPORT_URL = TRAC_URL + 'report/%s?asc=1&format=csv' % REPORT_NO
-#
-#EASY_REPORT_URL = TRAC_URL + 'report/%s?asc=1&format=csv' % EASY_NO
-#######################################################################
-
-#######################################################################
-# Again, pretty sure this is not necessary for anybody but here we go
-#######################################################################
-#easy_data = urllib.urlopen(EASY_REPORT_URL)
-#reader = csv.DictReader(easy_data)
-#easy_tickets = set()
-#for row in reader:
-#    if row.get('ticket'):
-#        easy_tickets.add(row['ticket'])
-#######################################################################
 
 def trac_to_gh(text):
     """Right, fun goes here. If you aspire to write magic syntax conversion
@@ -100,15 +72,14 @@ def github_time(date):
     Only necessary for comments, the tickets are extracted per web
     and thus do not rely on a date representation in trac's db.
     """
-    date = int(date) // 1000000
-    time = datetime.datetime.fromtimestamp(date)
-    return time.strftime('%Y-%m-%dT%H:%M:%S+01:00')
+    time = DateTime(str(date))
+    return time.ISO8601()
 
 def massage_comment(ticket, date, author, body):
     """Expands the ticket comment list for *ticket* with a json comment
     representation of *date*, *author* (mapped to github) and text *body*.
     """
-    body = trac_to_gh(body.decode('utf-8'))
+    body = trac_to_gh(body)
 
     # Not sure whether we have a related github account for that user.
     if USERNAMES.get(author):
@@ -194,37 +165,38 @@ def main():
     # Gather information about our tickets (mainly assembles comment list)
     #######################################################################
     # Stores a list of comments for each ticket by ID
-    comments = collections.defaultdict(list)
-    comment_rows = csv.reader(open(CSVFILE, 'rb'),
-                              delimiter=CSVDELIM, quotechar=CSVESCAPE)
-    for row in comment_rows:
-        try:
-            ticket, date, author, body = row
-        except: # malformed ticket query, fix in trac or the csv file!
-            print '/!\\ Please at check this csv row: /!\\\n', row
-            continue
-        if not ticket: # nothing we can do if there is no ticket to assign
-            continue
-        dct = massage_comment(ticket, date, author, body)
-        comments[ticket].append(dct) # defaultdict, append always works
+
+    #prock - load pickle file here and generate a row
+    f = open(TICKET_FILE, 'rb')
+    tickets = pickle.load(f)
+    f.close()
+
+    comment_coll = collections.defaultdict(list)
+
+    for ticketid in tickets:
+        comments = tickets[ticketid][4]
+        for comment in comments:
+            if ("comment" in comment):
+                dct = massage_comment(ticketid, comment[0],comment[1],comment[4])
+                comment_coll[ticketid].append(dct)
 
     #######################################################################
     # Write the ticket comments to json files indicating their parent issue
     #######################################################################
-    for ticket, data in comments.iteritems():
+    for ticket, data in comment_coll.iteritems():
         with open(ISSUES_PATH % ticket, 'w') as f:
             json.dump(data, f, indent=5)
 
     #######################################################################
     # Write the actual ticket data to separate json files (GitHub API v3)
     #######################################################################
-    csv_data = urllib.urlopen(TRAC_REPORT_URL)
-    ticket_data = csv.DictReader(csv_data)
-    for row in ticket_data:
-        if not (row.get('summary') and row.get('ticket')):
-            continue
-        with open(ISSUES_PATH % row['ticket'], 'w') as f:
-            write_issue(row, f)
+#    csv_data = urllib.urlopen(TRAC_REPORT_URL)
+#    ticket_data = csv.DictReader(csv_data)
+#    for row in ticket_data:
+#        if not (row.get('summary') and row.get('ticket')):
+#            continue
+#        with open(ISSUES_PATH % row['ticket'], 'w') as f:
+#            write_issue(row, f)
 
     #######################################################################
     # Finally, dump all milestones and the related data. This script is not
